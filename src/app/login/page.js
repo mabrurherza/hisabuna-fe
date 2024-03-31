f460a6ee13a0a104168c58e55e22afbac650dd15
Database19 <dedeprojectdev@gmail.com>
2024-03-31 16:31:55 +0700

    update

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -29,10 +29,13 @@ export default function LoginPage() {
     const handleSignIn = async () => {
         try {
             await axios.get(process.env.NEXT_PUBLIC_URLPROD + "/sanctum/csrf-cookie", {});
+            console.log(email)
+            console.log(password)
             const response = await axios.post(process.env.NEXT_PUBLIC_URLPROD + "/api/login", {
                 email: email,
                 password: password
             });
+            console.log(response)
 
             if(response.data.status == true){
                 localStorage.setItem('authToken', response.data.access_token);
